5b1e68b332f0bc6f6c6e1cfb41c1b9c554cf6b3e
Denys Kibalenko <dennieking87@gmail.com>
2025-05-15 19:16:29 +0200

    Feat: add `MovieBase`, `MoveiCreate`, `MovieUpdate` & `MovieRead` pydantic models with basic fild validation to src/shemas/movies.py

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,29 @@
-# Write your code here
+from decimal import Decimal
+
+from pydantic import BaseModel, ConfigDict, PastDate
+
+from database.models import MovieStatusEnum
+
+
+class MovieBase(BaseModel):
+    name: str
+    date: PastDate
+    score: float
+    overview: str
+    status: MovieStatusEnum
+    budget: Decimal
+    revenue: float
+
+
+class MovieCreate(MovieBase):
+    pass
+
+
+class MovieUpdate(MovieBase):
+    pass
+
+
+class MovieRead(MovieBase):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
