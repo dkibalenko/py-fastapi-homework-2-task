715683368a8150600a2914ef8483d6f1dafe78ce
Denys Kibalenko <dennieking87@gmail.com>
2025-05-22 17:28:51 +0200

    Feat: add `MovieCreateSchema` with custom validation of `date` & `country` fields
    - Add custom validation to `date` field to ensure it can not be more than a year in future
    - Remove `id: int` field from `CountrySchema`, `GenreSchema`, `ActorSchema`, `LanguageSchema`

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,16 +1,21 @@
 from typing import Optional
 
-from datetime import date
+from datetime import date, timedelta
 
 from decimal import Decimal
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    field_validator
+)
+import pycountry
 
 from database.models import MovieStatusEnum
 
 
 class CountrySchema(BaseModel):
-    id: int
     code: str
     name: Optional[str]
 
@@ -18,18 +23,56 @@ class CountrySchema(BaseModel):
 
 
 class GenreSchema(BaseModel):
-    id: int
     name: str
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class ActorSchema(BaseModel):
-    id: int
     name: str
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class LanguageSchema(BaseModel):
-    id: int
     name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class MovieCreateSchema(BaseModel):
+    name: str = Field(max_length=255)
+    date: date
+    score: float = Field(ge=0, le=100)
+    overview: str
+    status: MovieStatusEnum
+    budget: Decimal = Field(ge=0)
+    revenue: float = Field(ge=0)
+    country: str
+    genres: list[str]
+    actors: list[str]
+    languages: list[str]
+
+    @field_validator("date")
+    def validate_date_not_exceed_one_year_future(cls, value):
+        max_date = date.today() + timedelta(days=365)
+
+        if value > max_date:
+            raise ValueError("Date cannot exceed one year from today.")
+        return value
+
+    @field_validator("country")
+    def validate_country_iso_alpha3_code(cls, value):
+        alpha3_codes = [country.alpha_2 for country in pycountry.countries]
+
+        if value not in alpha3_codes:
+            raise ValueError(
+                "Country code must be 'ISO 3166-1 alpha-3' format"
+            )
+        return value
+
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class MovieDetailSchema(BaseModel):
@@ -41,7 +84,7 @@ class MovieDetailSchema(BaseModel):
     status: MovieStatusEnum
     budget: Decimal
     revenue: float
-    country_id: CountrySchema
+    country: CountrySchema
     languages: list[LanguageSchema]
     genres: list[GenreSchema]
     actors: list[ActorSchema]
