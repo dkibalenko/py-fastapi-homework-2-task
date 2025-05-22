00c17a097eb94aab14df35437e491cff65c3e04f
Denys Kibalenko <dennieking87@gmail.com>
2025-05-22 17:35:14 +0200

    Feat: add `get_or_create_related_object`, `get_list_of_related_objects`, `create_movie` async functions to src/crud/movies.py
    - Add `get_or_create_related_object` for creating related objects or getting them from database
    - Add `get_list_of_related_objects` for creating a list of related objects that are created or got from database
    - Add `create_movie` for creating a movie and all related objects, returning the full movie with relationships
    - Add `create_movie` to `from crud.movies import` statement in src/crud/__init__.py

--- src/crud/__init__.py
+++ src/crud/__init__.py
@@ -1,1 +1,1 @@
-from crud.movies import get_movie_list, generate_pagination_links
+from crud.movies import get_movie_list, generate_pagination_links, create_movie
--- src/crud/movies.py
+++ src/crud/movies.py
@@ -1,8 +1,9 @@
-from fastapi import HTTPException
-from sqlalchemy import func, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
-from database.models import MovieModel
+from database.models import MovieModel, Base, CountryModel, GenreModel, ActorModel, LanguageModel
+from schemas.movies import MovieCreateSchema, MovieDetailSchema
 
 
 def generate_pagination_links(
@@ -46,3 +47,114 @@ async def get_movie_list(
     movies = result.scalars().all()
 
     return movies
+
+
+async def get_or_create_related_object(
+    db: AsyncSession,
+    model: Base,
+    related_model_name: str,
+    field: str = None
+) -> Base:
+    """Get or create a related object by name or code."""
+    if not field:
+        field = (
+            "code"
+            if isinstance(related_model_name, str)
+            and len(related_model_name) == 2
+            else "name"
+        )
+    filter_kwargs = {field: related_model_name}
+    result = await db.execute(select(model).filter_by(**filter_kwargs))
+
+    existing_object = result.scalars().first()
+
+    if existing_object:
+        return existing_object
+
+    instance = model(**filter_kwargs)
+    db.add(instance)
+    await db.flush()
+    return instance
+
+
+async def get_list_of_related_objects(
+    db: AsyncSession,
+    model: Base, 
+    related_model_names: list[str]
+) -> list[Base]:
+    """Create a list of related objects."""
+    related_objects = [
+        await get_or_create_related_object(
+            db=db,
+            model=model,
+            related_model_name=name
+        )
+        for name in related_model_names
+    ]
+
+    return related_objects
+
+
+async def create_movie(
+    db: AsyncSession,
+    movie: MovieCreateSchema
+) -> MovieDetailSchema:
+    """
+    Create a movie and all related objects, returning the full movie
+    with relationships. Ensures atomicity of the transaction.
+    """
+    async with db.begin():
+        country = await get_or_create_related_object(
+            db=db,
+            model=CountryModel,
+            related_model_name=movie.country
+        )
+        genres_objects = await get_list_of_related_objects(
+            db=db,
+            model=GenreModel,
+            related_model_names=movie.genres
+        )
+        actors_objects = await get_list_of_related_objects(
+            db=db,
+            model=ActorModel,
+            related_model_names=movie.actors
+        )
+        languages_objects = await get_list_of_related_objects(
+            db=db,
+            model=LanguageModel,
+            related_model_names=movie.languages
+        )
+
+        db_movie = MovieModel(
+            name=movie.name,
+            date=movie.date,
+            score=movie.score,
+            overview=movie.overview,
+            status=movie.status,
+            budget=movie.budget,
+            revenue=movie.revenue,
+            country=country,
+            genres=genres_objects,
+            actors=actors_objects,
+            languages=languages_objects
+        )
+
+        db.add(db_movie)
+        await db.flush()
+        await db.refresh(db_movie)
+    
+    query = (
+        select(MovieModel)
+        .options(
+            joinedload(MovieModel.country),
+            joinedload(MovieModel.genres),
+            joinedload(MovieModel.actors),
+            joinedload(MovieModel.languages),
+        )
+        .where(MovieModel.id == db_movie.id)
+    )
+
+    result = await db.execute(query)
+    movie = result.unique().scalar_one()
+
+    return MovieDetailSchema.model_validate(movie)
